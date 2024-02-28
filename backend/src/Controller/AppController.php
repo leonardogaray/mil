274956fce3c29e29c12ddb9d711ca71bae1b5388
Leonardo Garay <lgaray@zennovia.com>
2024-02-28 17:23:12 -0300

    se modifica las rutas del proyecto angular

---
+++ backend/src/Controller/AppController.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AppController extends AbstractController
+{
+    #[Route('/build/app', name:'app', methods:['GET'])]
+    public function index(): Response
+    {       
+        return $this->render('app/index.html.twig', []);
+    }
+}
